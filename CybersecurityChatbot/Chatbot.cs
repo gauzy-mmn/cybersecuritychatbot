96e9ce13b0f2bb870c96473a1298935412ff62ea
Mogale Nchabeleng <mogale.m@icloud.com>
2026-06-26 23:29:21 +0200

    Update Chatbot version to 3.1
    Updated the `Version` property in the `Chatbot` class from "2.0"
    to "3.1" within the `CybersecurityChatbot` namespace. This
    version bump likely reflects new features, bug fixes, or other
    enhancements to the chatbot's functionality.

--- CybersecurityChatbot/Chatbot.cs
+++ CybersecurityChatbot/Chatbot.cs
@@ -17,7 +17,7 @@ namespace CybersecurityChatbot
         {
             BotName = "CyberSecBot";
             UserName = "";
-            Version = "2.0";
+            Version = "3.1";
         }
 
         public void PlayVoiceGreeting()
